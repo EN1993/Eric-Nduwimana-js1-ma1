4030bc643d3dec4f9f991bb6a7b0d96b97ce5efe
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-03-25 13:24:43 +0100

    add

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Answer question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i=0; i < paragraphs.length; i++);
+
+function paragraphsColor (){
+  
+  paragraphs.style.color = "red";
+  
+  };
+ //Answer question 6
+
+ const resultsContainer = document.querySelector(".results");
+
+ function changeResults() {
+   resultsContainer.innerHTML = "<p>New paragraph</p>";
+   resultsContainer.style.color = "yellow";
+
+ };
+
+
+
+
+
