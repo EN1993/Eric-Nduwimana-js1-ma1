8ced3ee6c1ab57d40739be2dacd68a98bdeac6de
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-03-26 11:40:46 +0100

    add

--- js/script.js
+++ js/script.js
@@ -2,24 +2,91 @@
 
 const paragraphs = document.querySelectorAll("p");
 
-for (let i=0; i < paragraphs.length; i++);
-
-function paragraphsColor (){
+console.log(paragraphs);
+ 
+ for (let i=0; i < paragraphs.length; i++){
   
-  paragraphs.style.color = "red";
+ paragraphs[i].style.color = "red"; 
+ };
+ 
+ 
   
-  };
  //Answer question 6
 
  const resultsContainer = document.querySelector(".results");
 
- function changeResults() {
    resultsContainer.innerHTML = "<p>New paragraph</p>";
-   resultsContainer.style.color = "yellow";
+   resultsContainer.style.background = "yellow";
 
- };
+ 
+
+//Answer question 7
+
+function animalList (list) {
+
+  const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+  for (i = 0; i < cats.length; i++){
+    console.log(cats[i].name);
+  }
+}
+
+animalList("list");
+
+// Answer question 8
+
+const assignCatsConteiner = document.querySelector (".cat-container");
+
+function createCats(cats) {
+  const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
 
 
 
+for (let i = 0; i < cats.length; i++) {
+  
+let html = "";
 
+let unkownAge =  "Age unknown"  ;
+if (cats[i].age !== cats[i].age) {
+  unkownAge = cats[i].age;
+}
+ 
+html +=  '<div class="pet">'
+               
+            "<h5> §{cats[i].name}</h5> "
+          
+            "<p> §{cats[i].age}</p>"
+              
+          '</div>';
+        
 
+}
+return html
+}
+
+createCats(cats);
+assignCatsConteiner.innerHTML = html;
